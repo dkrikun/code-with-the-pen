4a02c40e5cffdedf6ef65b9aee93e4373d78d68a
Daniel Krikun <krikun.daniel@gmail.com>
2018-12-19 10:53:10 +0200

    Etc

--- code_with_the_pen/InkTextAdornment.cs
+++ code_with_the_pen/InkTextAdornment.cs
@@ -7,44 +7,17 @@ using Microsoft.VisualStudio.Text.Formatting;
 
 namespace code_with_the_pen
 {
-    /// <summary>
-    /// InkTextAdornment places red boxes behind all the "a"s in the editor window
-    /// </summary>
     internal sealed class InkTextAdornment
     {
-        /// <summary>
-        /// The layer of the adornment.
-        /// </summary>
         private readonly IAdornmentLayer layer;
-
-        /// <summary>
-        /// Text view where the adornment is created.
-        /// </summary>
         private readonly IWpfTextView view;
 
-        /// <summary>
-        /// Adornment brush.
-        /// </summary>
         private readonly Brush brush;
-
-        /// <summary>
-        /// Adornment pen.
-        /// </summary>
         private readonly Pen pen;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="InkTextAdornment"/> class.
-        /// </summary>
-        /// <param name="view">Text view to create the adornment for</param>
         public InkTextAdornment(IWpfTextView view)
         {
-            if (view == null)
-            {
-                throw new ArgumentNullException("view");
-            }
-
             this.layer = view.GetAdornmentLayer("InkTextAdornment");
-
             this.view = view;
             this.view.LayoutChanged += this.OnLayoutChanged;
 
@@ -54,19 +27,11 @@ namespace code_with_the_pen
 
             var penBrush = new SolidColorBrush(Colors.Red);
             penBrush.Freeze();
+
             this.pen = new Pen(penBrush, 0.5);
             this.pen.Freeze();
         }
 
-        /// <summary>
-        /// Handles whenever the text displayed in the view changes by adding the adornment to any reformatted lines
-        /// </summary>
-        /// <remarks><para>This event is raised whenever the rendered text displayed in the <see cref="ITextView"/> changes.</para>
-        /// <para>It is raised whenever the view does a layout (which happens when DisplayTextLineContainingBufferPosition is called or in response to text or classification changes).</para>
-        /// <para>It is also raised whenever the view scrolls horizontally or when its size changes.</para>
-        /// </remarks>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event arguments.</param>
         internal void OnLayoutChanged(object sender, TextViewLayoutChangedEventArgs e)
         {
             foreach (ITextViewLine line in e.NewOrReformattedLines)
@@ -75,20 +40,17 @@ namespace code_with_the_pen
             }
         }
 
-        /// <summary>
-        /// Adds the scarlet box behind the 'a' characters within the given line
-        /// </summary>
-        /// <param name="line">Line to add the adornments</param>
         private void CreateVisuals(ITextViewLine line)
         {
             IWpfTextViewLineCollection textViewLines = this.view.TextViewLines;
 
-            // Loop through each character, and place a box around any 'a'
+            // loop through each character, and place a box around any 'a'
             for (int charIndex = line.Start; charIndex < line.End; charIndex++)
             {
                 if (this.view.TextSnapshot[charIndex] == 'a')
                 {
-                    SnapshotSpan span = new SnapshotSpan(this.view.TextSnapshot, Span.FromBounds(charIndex, charIndex + 1));
+                    SnapshotSpan span = new SnapshotSpan(this.view.TextSnapshot,
+                        Span.FromBounds(charIndex, charIndex + 1));
                     Geometry geometry = textViewLines.GetMarkerGeometry(span);
                     if (geometry != null)
                     {
@@ -107,7 +69,8 @@ namespace code_with_the_pen
                         Canvas.SetLeft(image, geometry.Bounds.Left);
                         Canvas.SetTop(image, geometry.Bounds.Top);
 
-                        this.layer.AddAdornment(AdornmentPositioningBehavior.TextRelative, span, null, image, null);
+                        this.layer.AddAdornment(AdornmentPositioningBehavior.TextRelative,
+                            span, null, image, null);
                     }
                 }
             }
